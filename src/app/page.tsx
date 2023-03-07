f971fa26a603fc0fd96411cea54c017269663b7d
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2023-03-07 03:57:00 +0100

    correct dependencies, import chakra

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+'use client'
+
+import { Text } from '@chakra-ui/react'
+
+import './styles/globals.css'
+
 export default function Home() {
-  return <main>Main</main>;
+  return (
+    <div>
+      Home<Text>No to mam tekst</Text>
+    </div>
+  )
 }
