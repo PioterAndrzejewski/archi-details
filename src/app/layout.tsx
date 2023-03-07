24d1db004ee7bb82743e210affa70dd8100fef58
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2023-03-07 04:26:07 +0100

    add nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,18 @@
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
 
+import { Navigation } from '@/components/navigation/Navigation'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <head />
       <body>
         <CacheProvider>
-          <ChakraProvider>{children}</ChakraProvider>
+          <ChakraProvider>
+            <Navigation />
+            {children}
+          </ChakraProvider>
         </CacheProvider>
       </body>
     </html>
